77a5566c8674b4646fd88fe72012eed72dc7b690
Nathalia Barbosa <nfab@cin.ufpe.br>
2024-05-16 21:05:55 -0300

    alterações nas funções

--- nimrod/test_suite_generation/generators/codellama_test_suite_generator.py
+++ nimrod/test_suite_generation/generators/codellama_test_suite_generator.py
@@ -39,6 +39,81 @@ class CodellamaTestSuiteGenerator(TestSuiteGenerator):
 
         return class_names
 
+    def _create_method_list(self, methods: "List[str]"):
+        rectified_methods = [self._convert_method_signature(
+            method) for method in methods]
+        return (":").join(rectified_methods)
+
+    def _convert_method_signature(self, meth_signature: str) -> str:
+        method_return = ""
+        try:
+            method_return = meth_signature.split(")")[1]
+        except Exception as e:
+            print(e)
+        meth_name = meth_signature[:meth_signature.rfind("(")]
+        meth_args = meth_signature[meth_signature.find(
+            "(") + 1:meth_signature.rfind(")")].split(",")
+        asm_meth_format = self._asm_based_method_method_descriptor(
+            meth_args, method_return)
+
+        return meth_name+asm_meth_format
+
+    # See at: https://asm.ow2.io/asm4-guide.pdf -- Section 2.1.3 and 2.1.4
+    # Java type Type descriptor
+    # boolean Z
+    # char C
+    # byte B
+    # short S
+    # int I
+    # float F
+    # long J
+    # double D
+    # Object Ljava/lang/Object;
+    # int[] [I
+    # Object[][] [[Ljava/lang/Object;
+    def _asm_based_method_method_descriptor(self, method_arguments, method_return):
+        result = '('
+        for arg in method_arguments:
+            arg = arg.strip()
+            result = result + self._asm_based_type_descriptor(arg)
+        result = result + ')'
+        result = result + self._asm_based_type_descriptor(method_return)
+        return result
+
+    def _asm_based_type_descriptor(self, arg):
+        result = ''
+        if '[]' in arg:
+            result = result + '['
+            arg = arg.replace('[]', '')
+
+        if arg == '':
+            result = result + ''
+        elif arg == 'int':
+            result = result + 'I'
+        elif arg == 'float':
+            result = result + 'F'
+        elif arg == 'boolean':
+            result = result + 'Z'
+        elif arg == 'char':
+            result = result + 'C'
+        elif arg == 'byte':
+            result = result + 'B'
+        elif arg == 'short':
+            result = result + 'S'
+        elif arg == 'long':
+            result = result + 'J'
+        elif arg == 'double':
+            result = result + 'D'
+        elif arg == 'void':
+            result = result + 'V'
+        elif arg == 'String':
+            result = result + 'Ljava/lang/String;'
+        else:
+            temp = "L" + arg.replace('.', '/') + ';'
+            result = result + temp
+
+        return result
+
     def generate_prompts(self, prompts_file, class_name, methods, code):
         for method in methods:
             temp_method = method.split("(")[0]
@@ -66,12 +141,8 @@ class CodellamaTestSuiteGenerator(TestSuiteGenerator):
                     modified_lines.append("import org.junit.runners.MethodSorters;\n\n")
                     print(f"Generating prompt for method {method}")
                     modified_lines.append(f"@FixMethodOrder(MethodSorters.NAME_ASCENDING)\n")
-                    modified_lines.append(f"public class {class_name}Test {{\n")
-                    modified_lines.append(f"    public static boolean debug = false;\n\n")
-                    modified_lines.append(f"    @Test\n")
-                    modified_lines.append(f"    public void {temp_method}_001() throws Throwable {{\n")
-                    modified_lines.append(f"        if (debug) {{\n")
-                    modified_lines.append(f"            // test here\n")
+                    modified_lines.append(f"public class {class_name.split('.')[-1]}Test {{\n")
+                    modified_lines.append(f"//continue with code only:\n")
                     modified_lines.append(f"\"\"\"")
                 
                     with open(prompts_file, "w") as file:
@@ -113,7 +184,7 @@ class CodellamaTestSuiteGenerator(TestSuiteGenerator):
             if not os.path.exists(dir):
                 os.makedirs(dir)
 
-        output_file_path = f"{dir}/{output_file_name}.txt"
+        output_file_path = f"{dir}/{output_file_name}.java"
         with open(output_file_path, "w") as f:
             f.write(prompt + output)
 
@@ -154,9 +225,9 @@ class CodellamaTestSuiteGenerator(TestSuiteGenerator):
         cm = self.create_chat_module(mpath, lpath, model, lib)
 
         for i, prompt in enumerate(prompts_list):
-            for j in range(0, 2):
+            for j in range(0, 10):
                 print(f"----------------------------- Generating output {i}-{j}")
-                output_file_name = f"output{i}-{j}-{class_name}-{branch}"
+                output_file_name = f"{i}{j}-{class_name.replace('.', '_')}-{branch}"
                 output = self.generate_output(cm, prompt)
                 self.save_output(prompt, output, output_path, output_file_name)
                 self.reset_chat_module(cm)
